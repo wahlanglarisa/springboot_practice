e9b657390af416e882fc3b5d06c8284edfa0dc68
Larisa Wahlang <wahlanglarisa@gmail.com>
2025-07-28 13:50:29 +0530

    Added new tables

---
+++ student/student/src/main/java/com/example/student/model/Class_Course.java
@@ -0,0 +1,33 @@
+package com.example.student.model;
+
+import java.sql.Time;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="class")
+public class Class_Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private Time time;
+	
+	private long semester;
+}
+ 
