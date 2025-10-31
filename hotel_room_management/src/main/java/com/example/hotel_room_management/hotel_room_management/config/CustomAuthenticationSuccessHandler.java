883365180c33ecb84114de7b1a4271c79450ea55
Larisa Wahlang <wahlanglarisa@gmail.com>
2025-10-31 16:09:34 +0530

    created new project

---
+++ hotel_room_management/src/main/java/com/example/hotel_room_management/hotel_room_management/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,38 @@
+package com.example.hotel_room_management.hotel_room_management.config;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+//configure redirects
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		System.out.println("In authentication function");
+		String redirectURL = request.getContextPath();
+
+		if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Student"))) {
+			redirectURL = "/student/studentHomepage";
+		}
+		if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Professor"))) {
+			redirectURL = "/professor/professorHomepage";
+		}
+		if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Admin"))) {
+			redirectURL ="/admin/adminPortal/1?sortField=email&sortDir=asc&role=";
+		}
+		if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Head Of Department"))) {
+			redirectURL ="/hod/hodPortal";
+		}
+		;
+		
+		// TODO Auto-generated method stub
+		response.sendRedirect(redirectURL);
+	}
+
+}
