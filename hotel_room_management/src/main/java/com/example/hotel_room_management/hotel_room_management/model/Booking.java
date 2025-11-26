6dc277badda2ad78eda75dbc980e31b7c3267adf
Larisa Wahlang <wahlanglarisa@gmail.com>
2025-11-26 16:15:29 +0530

    Created tables booking and room

---
+++ hotel_room_management/src/main/java/com/example/hotel_room_management/hotel_room_management/model/Booking.java
@@ -0,0 +1,4 @@
+package com.example.hotel_room_management.hotel_room_management.model;
+public class Booking {
+    
+}
--- hotel_room_management/src/main/java/com/example/hotel_room_management/hotel_room_management/model/Room.java
+++ hotel_room_management/src/main/java/com/example/hotel_room_management/hotel_room_management/model/Room.java
@@ -1,5 +1,28 @@
 package com.example.hotel_room_management.hotel_room_management.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public Long roomNo;
+    public Double ratePerNight;
+    public String status;
 
 }
